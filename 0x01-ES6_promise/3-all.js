323f8bc75636495bb220a530ae315e974957744c
James Kariuki <karisjaymo99@gmail.com>
2024-04-27 11:55:37 +0000

    Task 3, Handle multiple successful promises

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,12 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  const promise1 = uploadPhoto();
+  const promise2 = createUser();
+
+  Promise.all([promise1, promise2])
+    .then((data) => {
+      console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`);
+    })
+    .catch(() => { console.log('Signup system offline'); });
+}
